65dd3b862ee5f443a5d463f3e7a8e189d84dae4c
rabahdouasii@yahoo.fr <rabahdouassi@yahoo.fr>
2025-06-08 22:13:09 +0200

    Ton message de mise à jour

--- config/hoggar.php
+++ config/hoggar.php
@@ -4,5 +4,4 @@ return  [
     'storage_disk' => env('HOGGAR_STORAGE_DISK', 'public'),
     'storage_url' => env('HOGGAR_STORAGE_URL', 'http://127.0.0.1:8000/storage/')
     'company' =>'My Company',
-    'middlewareList' => ['auth'],
 ];
